c0a2d96cc9565bc11b67281205b83bc6844ab076
Ashutosh Malviya <ashutoshmalviya1000@gmail.com>
2022-01-10 12:17:19 +0530

    Add Cards

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Intro from './components/Intro';
+import Card from './components/Card';
+import data from './data';
 
 function App() {
+  let cards = data.map(item => {
+    return (
+      <Card
+        key= {item.id}
+        {...item}
+      />
+    )
+  })
   return (
     <div className="App">
       <Navbar/>
       <Intro/>
+      <section className="cards-list">
+        {cards}
+      </section>
     </div>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,32 @@
+export default function Card(props) {
+    let badgeText
+    if (props.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    } else if (props.location === "Online") {
+        badgeText = "ONLINE"
+    }
+    
+    return (
+        <div className="card">
+            {
+                badgeText && 
+                <div className="card--badge">{badgeText}</div>
+            }
+            <img 
+                src={require(`../images/${props.coverImg}`)} 
+                alt="card-img"
+                className="card--image" 
+            />
+            <div className="card--stats">
+                <img src={require("../images/star.png")} className="card--star" alt="star-icon" />
+                <span>{props.stats.rating}</span>
+                <span className="gray">({props.stats.reviewCount}) • </span>
+                <span className="gray">{props.location}</span>
+            </div>
+            <p className="card--title">{props.title}</p>
+            <p className="card--price">
+                <span className="bold">From ${props.price}</span> / person
+            </p>
+        </div>
+    )
+}
